786fe212feeb837fd991bba3b33c6297bc347a1e
Gevorgyan-Azat <hz2020@yandex.ru>
2022-11-16 02:36:55 +0300

    Завершен блок фильтрации

--- src/main/java/framework/pages/ResultPage.java
+++ src/main/java/framework/pages/ResultPage.java
@@ -2,6 +2,7 @@ package framework.pages;
 
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -13,65 +14,54 @@ import java.util.Objects;
 
 public class ResultPage extends BasePage {
 
-    @FindBy(xpath = "//div[@class='left-filters']//label[@class='ui-checkbox']")
-    private List<WebElement> checkBoxList;
-
     @FindBy(xpath = "//div[@class='apply-filters-float-btn']")
     private WebElement displayBtn;
 
-    @FindBy(xpath = "//span[@class='ui-collapse__link-text']")
-    private List<WebElement> filterBlockList;
-
     @FindBy(xpath = "//div[@class='left-filters__list']/div")
-    private List<WebElement> filterBlocksForScrollList;
+    private List<WebElement> filterBlocksList;
 
-    @FindBy(css = ".ui-checkbox__input:checked")
-    private List<WebElement> checkBoxChecked;
+    @FindBy(xpath = "//div[@class='left-filters__list']")
+    private WebElement leftFilters;
+
+    @FindBy(xpath = "//div[@class='products-page__top-block']")
+    private WebElement topBlock;
+
+    @FindBy(xpath = "//div[@class='left-filters__buttons-main']")
+    private WebElement leftFiltersButtonsBlock;
 
     {
         loadingFilterBlocks();
     }
 
-    public ResultPage() {
-    }
-
-    public void selectCheckBox (String checkBoxName) {
-        for (WebElement checkBox: checkBoxList) {
-            if (checkBox.getAttribute("textContent").contains(checkBoxName)) {
-                waitForJavascript();
-                scrollElementInCenter(checkBox);
-                waitUtilElementToBeClickable(checkBox).click();
-                waitUtilElementToBeVisible(displayBtn);
-                waitUtilElementToBeClickable(displayBtn).click();
-            }
-        }
-    }
-
-    public void findBlockAndSetValue(String filterBlockName, String fieldType, String blockFieldName, String expectedValue){
-        waitForJavascript();
+    public void findBlockAndSetValue(String filterBlockName, String fieldType, String blockFieldName, String expectedValue) {
         switch (fieldType) {
             case "checkBox":
                 changeCheckBoxValue(Objects.requireNonNull(selectFilterBlock(filterBlockName)), blockFieldName, Boolean.parseBoolean(expectedValue));
-                checkPickedFilterValueForCheckbox(blockFieldName);
+                checkPickedFilterValueForCheckbox(blockFieldName, expectedValue);
                 break;
             case "radioButton":
 
                 break;
             case "поля":
                 changeFieldValue(Objects.requireNonNull(selectFilterBlock(filterBlockName)), blockFieldName, expectedValue);
+                checkPickedFilterValueForField(blockFieldName, expectedValue);
                 break;
         }
     }
 
-    private WebElement selectFilterBlock(String filterBlockName){
-        for (WebElement filterBlock: filterBlockList) {
-            if(filterBlock.getAttribute("textContent").contains(filterBlockName)){
-                if(!filterBlock.findElement(By.xpath("./..")).getAttribute("class").contains("link_in")) {
-                    scrollElementInCenter(filterBlock.findElement(By.xpath("./..")));
-                    filterBlock.findElement(By.xpath("./..")).click();
-                    wait.until(ExpectedConditions.attributeContains(filterBlock.findElement(By.xpath("./..")), "class", "link_in"));
+    private WebElement selectFilterBlock(String filterBlockName) {
+        if (filterBlockName.equals("*")) {
+            return leftFilters;
+        }
+        for (WebElement filterBlock : filterBlocksList) {
+            if (filterBlock.getAttribute("textContent").contains(filterBlockName)) {
+                if (!filterBlock.getAttribute("class").equals("left-filters-toggle") &&
+                        !filterBlock.findElement(By.xpath("./a")).getAttribute("class").contains("link_in")) {
+                    scrollElementInCenter(filterBlock.findElement(By.xpath("./a")));
+                    filterBlock.findElement(By.xpath("./a")).click();
+                    wait.until(ExpectedConditions.attributeContains(filterBlock.findElement(By.xpath("./a")), "class", "link_in"));
                     Assert.assertTrue("Не удалось раскрыть блок фильтра под названием '" + filterBlockName + "'",
-                            filterBlock.findElement(By.xpath("./..")).getAttribute("class").contains("link_in"));
+                            filterBlock.findElement(By.xpath("./a")).getAttribute("class").contains("link_in"));
                 }
                 return filterBlock;
             }
@@ -82,27 +72,35 @@ public class ResultPage extends BasePage {
 
 
     private void changeCheckBoxValue(WebElement element, String checkBoxName, boolean expectedStatus) {
-        List<WebElement> filterBlockList = element.findElements(By.xpath("./../..//label"));
-        for(WebElement filterBlock: filterBlockList) {
-            if (filterBlock.getAttribute("textContent").contains(checkBoxName)) {
-                List<WebElement> checkBox = filterBlock.findElements(By.cssSelector(".ui-checkbox__input:checked"));
-                if(checkBox.size()<1 && expectedStatus) {
-                    scrollAndClickCheckBox(filterBlock);
-                    return;
-                } else if (checkBox.size()>0 && !expectedStatus){
-                    scrollAndClickCheckBox(filterBlock);
-                    return;
+        List<WebElement> checkBoxList = element.findElements(By.xpath(".//label[not(@style='display: none;')]"));
+        for (WebElement checkBox : checkBoxList) {
+            if (checkBox.getAttribute("textContent").contains(checkBoxName)) {
+                try {
+                    checkBox.findElement(By.cssSelector(".ui-checkbox__input:checked"));
+                    if (!expectedStatus) {
+                        scrollAndClickCheckBox(checkBox);
+                        return;
+                    }
+                } catch (NoSuchElementException ignore) {
+                    if (expectedStatus) {
+                        scrollAndClickCheckBox(checkBox);
+                        return;
+                    }
                 }
                 return;
             }
         }
     }
 
-    private void checkPickedFilterValueForCheckbox(String checkBoxName){
-        WebElement pickedFilter = driverManager.getDriver().findElement(By.xpath(String.format("//div[@class='top-filters']//div[@class='picked-filter' and contains(., '%s')]", checkBoxName)));
-        wait.until(ExpectedConditions.attributeContains(pickedFilter, "textContent", checkBoxName));
-        Assert.assertTrue("",
-                pickedFilter.getAttribute("textContent").contains(checkBoxName));
+    private void checkPickedFilterValueForCheckbox(String checkBoxName, String expectedValue) {
+        if (Boolean.parseBoolean(expectedValue)) {
+            try {
+                driverManager.getDriver().findElement(By.xpath(String.format("//div[@class='top-filters']//div[@class='picked-filter' and contains(., '%s')]", checkBoxName)));
+            } catch (NoSuchElementException ignore) {
+                Assert.fail("Результат поиска не отфильтрован по checkbox '" + checkBoxName + "'");
+            }
+        }
+
     }
 
     private void scrollAndClickCheckBox(WebElement element) {
@@ -113,8 +111,8 @@ public class ResultPage extends BasePage {
     }
 
     private void changeFieldValue(WebElement element, String fieldName, String value) {
-        List<WebElement> fieldsList = element.findElements(By.xpath("./../..//input[@placeholder]"));
-        for(WebElement field: fieldsList) {
+        List<WebElement> fieldsList = element.findElements(By.xpath(".//input[@placeholder]"));
+        for (WebElement field : fieldsList) {
             if (field.getAttribute("placeholder").contains(fieldName)) {
                 scrollElementInCenter(field);
                 waitUtilElementToBeClickable(field).click();
@@ -122,20 +120,27 @@ public class ResultPage extends BasePage {
                 field.sendKeys(value);
                 waitUtilElementToBeVisible(displayBtn);
                 waitUtilElementToBeClickable(displayBtn).click();
-                break;
+                return;
             }
         }
-        WebElement pickedFilter = driverManager.getDriver().findElement(By.xpath(String.format("//div[@class='top-filters']//div[@class='picked-filter' and contains(., '%s')]", value)));
-        wait.until(ExpectedConditions.attributeContains(pickedFilter, "textContent", value));
-        Assert.assertTrue("",
-                pickedFilter.getAttribute("textContent").contains(value));
+        Assert.fail("Не удалось найти поле '" + fieldName + "'");
+    }
+
+    private void checkPickedFilterValueForField(String fieldName, String expectedValue) {
+        try {
+            driverManager.getDriver().findElement(By.xpath(String.format("//div[@class='top-filters']//div[@class='picked-filter' and contains(., '%s') and contains(., '%s')]", fieldName, expectedValue)));
+        } catch (NoSuchElementException ignore) {
+            Assert.fail("Результат поиска не отфильтрован по полю '" + fieldName + "'");
+        }
     }
 
     private void loadingFilterBlocks() {
-        for (int i = 0; i < filterBlocksForScrollList.size()-1; i+=2){
-            List<WebElement> filterBlocks = new ArrayList<>(driverManager.getDriver().findElements(By.xpath("//div[@class='left-filters__list']/div")));
+        waitForJavascript();
+        for (int i = 0; i < filterBlocksList.size() - 1; i+=2) {
+            List<WebElement> filterBlocks = new ArrayList<>(filterBlocksList);
             scrollElementInCenter(filterBlocks.get(i));
         }
-        scrollElementInCenter(driverManager.getDriver().findElement(By.xpath("//div[@class='products-page__top-block']")));
+        scrollElementInCenter(leftFiltersButtonsBlock);
+        scrollElementInCenter(topBlock);
     }
 }
--- src/main/java/framework/steps/ResultPageStep.java
+++ src/main/java/framework/steps/ResultPageStep.java
@@ -8,10 +8,10 @@ public class ResultPageStep {
 
     PageManager pageManager = PageManager.getINSTANCE();
 
-    @И("^Изменить статус чекбокса (Рейтинг|В наличии) на (true|false)$")
-    public void selectCheckBox(String checkBoxName, boolean expectedStatus){
-        pageManager.getPage(ResultPage.class).selectCheckBox(checkBoxName);
-    }
+//    @И("^Изменить статус чекбокса (Рейтинг|В наличии) на (true|false)$")
+//    public void selectCheckBox(String checkBoxName, boolean expectedStatus){
+//        pageManager.getPage(ResultPage.class).selectCheckBox(checkBoxName);
+//    }
 
     @И("^В блоке (.+) изменить значение (checkBox|radioButton|поля) '(.+)' на (.+)$")
     public void selectBlock(String filterBlockName, String fieldType, String blockFieldName, String expectedValue){
